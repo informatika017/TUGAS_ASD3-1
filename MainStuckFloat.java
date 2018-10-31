750257950bb30c7a6c512dcbf770fcff8519f022
ismaild0217308 <43749027+ismaild0217308@users.noreply.github.com>
2018-10-31 22:12:04 +0800

    Create  MainStuckFloat.java

---
+++ MainStuckFloat.java
@@ -0,0 +1,14 @@
+package TUGAS_ASD3;
+
+public class MainStuckFloat {
+public static void main(String[] args) {
+		classstuckfloat sc=new classstuckfloat();
+			sc.push(1.2f);
+			sc.push(2.3f);
+			sc.push(3.4f);
+			sc.cetak();
+			float d0=sc.pop();
+			System.out.println(d0);
+			sc.cetak();
+	}
+}
