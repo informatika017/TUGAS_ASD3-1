5dd7d0554fc2019cf5c0eeb9577cbeb2cebc8082
ismaild0217308 <43749027+ismaild0217308@users.noreply.github.com>
2018-10-31 22:07:26 +0800

    Create MainQueueDouble.java

---
+++ MainQueueDouble.java
@@ -0,0 +1,15 @@
+package TUGAS_ASD3;
+import sun.misc.Queue;
+public class MainQueueDouble {
+
+	public static void main(String[] args) {
+		QueueDouble q = new QueueDouble();
+		q.cetak();
+		
+		q.insert(1.1);q.cetak();
+		q.insert(2.1);q.cetak();
+		q.insert(3.1);q.cetak();
+		q.insert(4.1);q.cetak();
+		
+	}
+}
