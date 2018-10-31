7ae735c98cd7271c1ed74d025ddcb4b6baa8b339
ismaild0217308 <43749027+ismaild0217308@users.noreply.github.com>
2018-10-31 22:00:59 +0800

    Create ArrayListInteger.java

---
+++ ArrayListInteger.java
@@ -0,0 +1,14 @@
+package TUGAS_ASD3;
+import java.util.ArrayList;
+
+public class ArrayListInteger {
+
+	public static void main(String[] args) {
+		ArrayList<Integer>lint = new ArrayList<Integer>();
+		lint.add(2);
+		lint.add(1);
+		lint.add(2);
+		System.out.println("MASIH HIDUP"+lint.toString());
+		System.out.println("SIZE : "+lint.size());	
+	}
+}
